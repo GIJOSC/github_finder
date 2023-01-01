d80e885301829a1d4a9949aab49de891dd05ca12
Gesiel Neves <neves.gesiel@gmail.com>
2023-01-01 10:54:41 -0300

    feat: created types

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { UserProps } from "../types/user";
+
+import Search from "../components/Search";
+
+import { useState } from "react";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [user, setUser] = useState<UserProps | null>(null);
+  return (
+    <div>
+      <Search />
+    </div>
+  );
 };
 
 export default Home;
